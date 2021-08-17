8510a06012d675d0f9f00be1abe32b6b5fad85b6
iamebube-ekpe <victorekpe11@gmail.com>
2021-08-17 19:09:59 +0100

    2nd Zuri Task: Resume

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+import os
+from  dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/send_msg', methods=['POST'])
+def send_msg():
+    if request.method == "POST":
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+
+        message = Message(subject,sender="victorekpe11@gmail.com",recipients=["victorekpe11@gmail.com"])
+        message.body = f""" From: {email} <Subject:{subject}> \n{msg} """ 
+        mail.send(message)
+
+        # success = "Your message has been sent!"
+        return redirect('/')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
